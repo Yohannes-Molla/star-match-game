0fb4574da957083c70dcdb8676a00c6b4cb960a6
yohannes <ymolla@paga.com>
2022-03-01 22:04:47 +0300

    star match game final

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import StarMatch from './components/StarMatch';
+
+function App() {
+  return (
+    <div className="App">
+      <StarMatch />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/state/useGameState.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+import utils from "../utils/utils";
+
+const useGameState = timeLimit => {
+  const [stars, setStars] = useState(utils.random(1, 9));
+  const [availableNums, setAvailableNums] = useState(utils.range(1, 9));
+  const [candidateNums, setCandidateNums] = useState([]);
+  const [secondsLeft, setSecondsLeft] = useState(timeLimit);
+
+  useEffect(() => {
+    if (secondsLeft > 0 && availableNums.length > 0) {
+      const timerId = setTimeout(() => setSecondsLeft(secondsLeft - 1), 1000);
+      return () => clearTimeout(timerId);
+    }
+  });
+
+  const setGameState = (newCandidateNums) => {
+    if (utils.sum(newCandidateNums) !== stars) {
+			setCandidateNums(newCandidateNums);
+    } else {
+      const newAvailableNums = availableNums.filter(
+        n => !newCandidateNums.includes(n)
+      );
+      setStars(utils.randomSumIn(newAvailableNums, 9));
+      setAvailableNums(newAvailableNums);
+      setCandidateNums([]);
+    }
+  };
+
+  return { stars, availableNums, candidateNums, secondsLeft, setGameState };
+};
+
+export default useGameState;
